436a4fc0e7f814aa0ef9a5e6bf6794c58c447aef
XXmmRR <Xorex50@protonmail.com>
2022-04-28 09:02:31 +0300

    init

---
+++ webapp/src/main/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+# Create your views here.
+from . import tasks
+
+
+def home(request):
+    tasks.download_a_cat.delay()
+    return HttpResponse('<h1>Грузим котика</h1>')
+
